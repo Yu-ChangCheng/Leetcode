152e4024c44d77263f935c380df4cb75667a7c05
Cheng, Yu-Chang <ycheng345@gatech.edu>
2023-04-30 23:54:36 -0500

    Intervals 919 Meeting room 2

---
+++ 919 meeting room 2.py
@@ -0,0 +1,36 @@
+from typing import (
+    List,
+)
+from lintcode import (
+    Interval,
+)
+
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    """
+    @param intervals: an array of meeting time intervals
+    @return: the minimum number of conference rooms required
+    """
+    def min_meeting_rooms(self, intervals: List[Interval]) -> int:
+        # Write your code here
+        start = sorted([i.start for i in intervals])
+        end = sorted([i.end for i in intervals])
+        s, e = 0, 0
+        res, count = 0, 0
+
+        while s < len(intervals):
+            if start[s] < end[e]:
+                count += 1
+                s += 1
+            else:
+                count -= 1
+                e += 1
+            res = max(res, count)
+        return res
