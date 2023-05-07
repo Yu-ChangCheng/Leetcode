dc50e88ca988995c0406f7fcb3fceedefbeb05cb
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2023-05-07 12:52:06 -0500

    Time: 31 ms (89.78%) | Memory: 13.7 MB (91.59%) - LeetSync

---
+++ valid-anagram/valid-anagram.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) !=len(t):
+            return False
+            
+        s_dict = defaultdict(int)
+        for c in s:
+            s_dict[c] += 1
+        
+        for c in t:
+            if c not in s_dict or s_dict[c] <=0:
+                return False
+            else:
+                s_dict[c] -= 1
+        return True
