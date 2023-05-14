c34f81a4fc67b96a9e0249967424501ca44a662f
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2023-05-14 16:42:41 -0500

    Time: 41 ms (76.60%) | Memory: 14.4 MB (28.78%) - LeetSync

--- longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -9,16 +9,14 @@ class Solution(object):
         elif len(s) == 1:
             return 1
 
+        'abcbd'
         charSet = set()
         l = 0
         res = 0
         for r in range(len(s)):
-            if s[r] not in charSet:
-                charSet.add(s[r])
-                res = max(res, r-l+1)
-            else:
-                while l < r and s[l] != s[r]:
-                    charSet.remove(s[l])
-                    l += 1
+            while s[r] in charSet:
+                charSet.remove(s[l])
                 l += 1
+            charSet.add(s[r])
+            res = max(res, r-l+1)
         return res
