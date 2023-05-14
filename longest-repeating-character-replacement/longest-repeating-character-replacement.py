fa2bc7c063620465b09d1b497894f4955f58b240
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2023-05-14 17:15:09 -0500

    Time: 97 ms (49.60%) | Memory: 14 MB (8.59%) - LeetSync

---
+++ longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def characterReplacement(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        count = defaultdict(int)
+        l = 0
+        res = 0
+
+        for r in range(len(s)):
+            count[s[r]] += 1
+            if (r-l+1) - max(count.values()) > k:
+                count[s[l]] -= 1
+                l += 1
+            res = max(res, r-l+1)
+
+        return res
+
+        
+        
