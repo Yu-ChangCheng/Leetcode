5c6a2d5ffc8da9f6cb1be5ccb37ba8dde67b8fa3
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2023-05-16 14:51:36 -0500

    Time: 82 ms (42.70%) | Memory: 13.8 MB (78.33%) - LeetSync

---
+++ permutation-in-string/permutation-in-string.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def checkInclusion(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: bool
+        """
+
+        s1_len, s2_len = len(s1), len(s2)
+        s1_counter = Counter(s1)
+        s2_counter = Counter(s2[:s1_len])
+
+        for i in range(s1_len, s2_len):
+            if s1_counter == s2_counter:
+                return True
+            s2_counter[s2[i-s1_len]] -= 1
+            if s2_counter[s2[i-s1_len]] == 0:
+                del s2_counter[s2[i-s1_len]]
+            s2_counter[s2[i]] += 1
+
+        return s1_counter == s2_counter  
+
