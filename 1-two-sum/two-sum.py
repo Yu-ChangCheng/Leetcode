28407ecd143885a3601f70618ceb7dde64d05bc9
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2023-08-18 20:34:00 -0500

    Time: 39 ms (86.03%) | Memory: 14.3 MB (36.00%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        hashmap = defaultdict(int)
+        for i, n in enumerate(nums):
+            
+            complement = target - n
+            if complement in hashmap:
+                return [hashmap[complement], i]
+            hashmap[n] = i
