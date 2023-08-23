440ed47b3e607a290a59f56aa0db5c1a0b0a5c78
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2023-08-23 13:12:04 -0500

    Time: 44 ms (72.69%) | Memory: 14.1 MB (65.26%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -6,9 +6,10 @@ class Solution(object):
         :rtype: List[int]
         """
         hashmap = defaultdict(int)
+
         for i, n in enumerate(nums):
-            
             complement = target - n
             if complement in hashmap:
                 return [hashmap[complement], i]
             hashmap[n] = i
+        
