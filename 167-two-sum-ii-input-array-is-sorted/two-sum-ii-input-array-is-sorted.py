864410a0085a3662017a817743a910d5cb7cd42c
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-01-16 16:07:21 -0600

    Time: 89 ms (70.70%) | Memory: 14.2 MB (55.56%) - LeetSync

---
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        l, r = 0 , len(numbers) - 1
+
+        while l < r:
+            if numbers[l] + numbers[r] < target:
+                l += 1
+            elif numbers[l] + numbers[r] > target:
+                r -= 1
+            else:
+                return [l+1, r+1]
