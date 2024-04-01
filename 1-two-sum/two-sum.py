8dde67b19f2ad3c2f875e97794a8dace530dc2fc
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-03-31 19:14:46 -0500

    Time: 35 ms (86.48%) | Memory: 12.7 MB (22.02%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,11 +5,12 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        hashmap = defaultdict(int)
+        dict_complement = defaultdict(int)
 
         for i in range(len(nums)):
             complement = target - nums[i]
-            if complement not in hashmap:
-                hashmap[nums[i]] = i
+            if complement not in dict_complement:
+                dict_complement[nums[i]] = i
             else:
-                return [i, hashmap[complement]]
+                return [dict_complement[complement],i]
+                
