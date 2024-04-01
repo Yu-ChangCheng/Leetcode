bbb0e474d5760082676c1eaa1b2851194bd7f891
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-04-01 10:59:18 -0500

    Time: 68 ms (33.35%) | Memory: 11.8 MB (13.15%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -4,19 +4,18 @@ class Solution(object):
         :type board: List[List[str]]
         :rtype: bool
         """
-        cols = defaultdict(set)
         rows = defaultdict(set)
+        cols = defaultdict(set)
         squares = defaultdict(set)
 
         for r in range(9):
             for c in range(9):
-                if board[r][c] == ".":
+                if board[r][c] ==".":
                     continue
-                if (board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in squares[(r//3, c//3)]):
+                if board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in squares[r//3,c//3]:
                     return False
-
                 rows[r].add(board[r][c])
                 cols[c].add(board[r][c])
-                squares[r//3,c//3].add(board[r][c])
+                squares[r//3, c//3].add(board[r][c])
                 
         return True
