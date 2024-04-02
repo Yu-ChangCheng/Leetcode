dee10db5150dcbe05b1373315480204a2b00ae41
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-04-01 22:05:46 -0500

    Time: 61 ms (70.38%) | Memory: 11.8 MB (13.13%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -10,12 +10,13 @@ class Solution(object):
 
         for r in range(9):
             for c in range(9):
-                if board[r][c] ==".":
+                if board[r][c] == ".":
                     continue
-                if board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in squares[r//3,c//3]:
+                if board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in squares[r//3, c//3]:
                     return False
+
                 rows[r].add(board[r][c])
                 cols[c].add(board[r][c])
                 squares[r//3, c//3].add(board[r][c])
-                
         return True
+
