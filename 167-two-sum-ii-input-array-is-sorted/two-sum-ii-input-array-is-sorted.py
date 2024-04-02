9122dd054b4edd6e7d43b45009347c1025094968
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-04-02 11:55:14 -0500

    Time: 89 ms (61.94%) | Memory: 12.5 MB (76.30%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -5,12 +5,15 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        l, r = 0 , len(numbers) - 1
+        l = 0
+        r = len(numbers) - 1
 
+        twosum = 0 
         while l < r:
-            if numbers[l] + numbers[r] < target:
-                l += 1
-            elif numbers[l] + numbers[r] > target:
+            twosum = numbers[l] + numbers[r]
+            if twosum > target:
                 r -= 1
+            elif twosum < target:
+                l += 1
             else:
                 return [l+1, r+1]
