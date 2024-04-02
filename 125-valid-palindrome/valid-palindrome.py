e839aff49a5ed4efd6b48b15ce2c3efe182c8fae
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-04-01 22:58:39 -0500

    Time: 25 ms (80.56%) | Memory: 12.3 MB (83.79%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -4,18 +4,16 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        l, r = 0, len(s) - 1
-        
+        l = 0 
+        r = len(s) - 1
 
         while l < r:
             while l < r and not s[l].isalnum():
                 l += 1
             while l < r and not s[r].isalnum():
                 r -= 1
-
-            if s[l].lower() != s[r].lower():
+            if l < r and s[l].lower() != s[r].lower():
                 return False
             l += 1
             r -= 1
-        
         return True
