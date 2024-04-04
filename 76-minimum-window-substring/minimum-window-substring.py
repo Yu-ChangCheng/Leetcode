a4adc68d1099b0508bc59e925a47d9e8f108b811
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-04-04 00:26:47 -0500

    Time: 96 ms (61.16%) | Memory: 15.1 MB (26.33%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -5,36 +5,30 @@ class Solution(object):
         :type t: str
         :rtype: str
         """
-        if not s or not t or len(s) < len(t):
-            return ''
+        if not s or not t or len(s) < len(t): return ''
+        countT = defaultdict(int)
+        window = defaultdict(int)
 
-        countT, window = defaultdict(int), defaultdict(int)
-        
         for c in t:
-            countT[c] += 1 
-
+            countT[c] += 1
+        
         have = 0
         need = len(countT)
-        res, resLen = [-1, -1], float('infinity') 
+        res, resLen = [-1, -1], float('infinity')
 
-        l = 0 
+        l = 0
         for r in range(len(s)):
             window[s[r]] += 1
-
             if s[r] in countT and window[s[r]] == countT[s[r]]:
                 have += 1
-            
-            while need == have:
+            while have == need:
                 if (r - l + 1) < resLen:
                     res = [l, r]
                     resLen = (r - l + 1)
-
                 window[s[l]] -= 1
                 if s[l] in countT and window[s[l]] < countT[s[l]]:
                     have -= 1
                 l += 1
         l, r = res
         return s[l:r+1]    
-            
-                
                 
