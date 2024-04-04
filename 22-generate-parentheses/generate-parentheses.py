d3de8f7a2167640e6706df4330edf7e9f76fbc3a
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-04-04 15:24:50 -0500

    Time: 21 ms (25.72%) | Memory: 11.7 MB (91.99%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -5,18 +5,19 @@ class Solution(object):
         :rtype: List[str]
         """
         path = ""
+        openN = 0
+        closeN = 0 
         res = []
 
-        def dfs(openN, closeN, path):
-            if openN == closeN == n:
+        def DFS(openN, closeN, path):
+            if n == openN == closeN:
                 res.append(path)
                 return
-            
-            if openN < n:
-                dfs(openN+1, closeN, path + "(")
+            if n >= openN and openN >= closeN:
+                DFS(openN+1, closeN, path + "(")
 
-            if closeN < openN:
-                dfs(openN, closeN+1, path + ")")
-
-        dfs(0,0,path)
+            if n >= closeN and closeN <= openN:
+                DFS(openN, closeN+1, path + ")")
+        
+        DFS(0,0,path)
         return res
