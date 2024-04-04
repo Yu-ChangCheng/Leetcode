09617234f83d4c0c3aebd65eacc3311ca791b25f
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-04-04 00:11:13 -0500

    Time: 60 ms (49.36%) | Memory: 12.2 MB (38.68%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -5,6 +5,7 @@ class Solution(object):
         :type s2: str
         :rtype: bool
         """
+
         s1_len, s2_len = len(s1), len(s2)
         s1_counter = Counter(s1)
         s2_counter = Counter(s2[:s1_len])
@@ -16,5 +17,4 @@ class Solution(object):
             if s2_counter[s2[i-s1_len]] == 0:
                 del s2_counter[s2[i-s1_len]]
             s2_counter[s2[i]] += 1
-
         return s1_counter == s2_counter
