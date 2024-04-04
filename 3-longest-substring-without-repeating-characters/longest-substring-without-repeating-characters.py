563266d77865f85e8d755eb402dfb19b99a6498d
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-04-04 00:00:30 -0500

    Time: 31 ms (78.69%) | Memory: 12.4 MB (52.93%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -4,19 +4,19 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+
         if len(s) == 0: return 0
         if len(s) == 1: return 1
 
         l = 0
         substring_set = set()
         res = 0
+
         for r in range(len(s)):
             while s[r] in substring_set:
                 substring_set.remove(s[l])
                 l += 1
-            res = max(res, r - l + 1)
             substring_set.add(s[r])
+            res = max(res, r - l + 1)
         return res
-            
-
-
+        
