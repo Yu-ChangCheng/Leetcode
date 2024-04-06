73bb40013aa3aef1bd6e8850708511e5f13b7722
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-04-06 17:23:18 -0500

    Time: 50 ms (96.29%) | Memory: 29.3 MB (93.37%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -9,39 +9,37 @@ class Solution(object):
         :type head: ListNode
         :rtype: None Do not return anything, modify head in-place instead.
         """
-        # slow and fast to find the midpoint to break into two list
+        # find the mid of the list using slow and fast pointer
         slow = head
         fast = head.next
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-        
-        # reset second to the first.next so it is the start of the second list
-        second = slow.next
-        # remove the link from first to the second list
-        prev = slow.next = None
+        # mid of the list is the slow.next
 
-        # reverse the second list
+        second = slow.next
+        slow.next = None # disconnect the first list and second list
+
+        prev = None
         while second:
             temp = second.next
             second.next = prev
             prev = second
             second = temp
-        
-        # merge two list
-        second = prev
+
+        # combine two list
+
         first = head
+        second = prev
 
-        # firstlist = [1,2,3]
-        # secondlist = [9,8,7]
-        # first = 1
-        # second = 9
+        while second: # be careful since list2 is shorter than list 1 so the last node would be from list1
+            temp1 = first.next
+            temp2 = second.next
+            first.next = second
+            second.next = temp1
+            first = temp1
+            second = temp2
+        
 
-        while second:
-            temp1 = first.next # 2
-            temp2 = second.next # 8
-            first.next = second # 1->9
-            second.next = temp1 # 1->9->2->8
-            first = temp1 # (first 2)
-            second = temp2 # (seecond 8)
+
 
