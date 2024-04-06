03a3c78962be9a3bc15caaa4f28660ff98baa07b
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-04-06 16:50:50 -0500

    Time: 22 ms (62.95%) | Memory: 12 MB (10.06%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -5,23 +5,34 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        l = 0 
+
+        # [4,5,6,7,0,1,2] left rotated list (nums[l] <= nums[m])
+        #. l    ,m,    r                    (nums[l] <= t1 <= nums[m]) we shift or r = m - 1
+        #.   t1.     t2                     l += 1
+ 
+        # [4,5,6,0,1,2,3] right rotated list nums[l] > nums[m]
+        #. l    ,m,    r                    (nums[m] <= t2 <= nums[r]) we shift or l = m + 1
+        #.   t1.     t2                     r -= 1
+
+        l = 0
         r = len(nums) - 1
-        
+
         while l <= r:
-            m = (l+r) // 2
+            m = (l + r) // 2
+
             if nums[m] == target:
                 return m
-            # we are on the left rotation
-            if nums[l] <= nums[m]:
+
+            if nums[l] <= nums[m]: # left rotated list
                 if nums[l] <= target <= nums[m]:
                     r = m - 1
                 else:
                     l = m + 1
-            else:
+            else: # right rotated list
                 if nums[m] <= target <= nums[r]:
                     l = m + 1
                 else:
                     r = m - 1
-        return -1    
-                
+        return -1
+            
+
