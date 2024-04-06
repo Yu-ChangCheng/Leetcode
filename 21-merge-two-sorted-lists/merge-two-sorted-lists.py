ac2a6f1a15b2c3e7a21bc9ec352040a7322118b1
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-04-06 17:09:19 -0500

    Time: 21 ms (43.35%) | Memory: 11.7 MB (32.05%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -10,20 +10,21 @@ class Solution(object):
         :type list2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        dummy = ListNode()
-        tail = dummy
+        tail = ListNode() # dummy is None return dummy.next
+        dummy = tail
 
         while list1 and list2:
-            if list1.val > list2.val:
-                tail.next = list2
-                list2 = list2.next
-            else:
+            if list1.val < list2.val:
                 tail.next = list1
                 list1 = list1.next
-            tail = tail.next
-        
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next # update the current pointer
+
         if list1:
             tail.next = list1
         if list2:
             tail.next = list2
+
         return dummy.next
