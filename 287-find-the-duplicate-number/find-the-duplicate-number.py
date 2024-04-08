dd0778c07f882b09673b0cfca6ae46a801551b81
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-04-08 16:58:08 -0500

    Time: 479 ms (63.12%) | Memory: 23.4 MB (87.47%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -4,16 +4,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        slow = fast = 0
+        slow, fast = 0, 0
+        # find the slow pointer location
         while True:
             slow = nums[slow]
             fast = nums[nums[fast]]
             if slow == fast:
                 break
-        
+        # nums = [1,3,4,2,2]
+        # 0->1->3->2->4->2->4->2->4
+        # find the intersection due to the distance between the start point to the goal point would be the same as the slow point to the goal point
         begin = 0
         while True:
             begin = nums[begin]
             slow = nums[slow]
-            if slow == begin:
+            if begin == slow:
                 return slow
