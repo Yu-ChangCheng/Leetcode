222db891682c75eb1f307dcbb4def686c953c943
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-04-10 16:44:59 -0500

    Time: 717 ms (86.83%) | Memory: 78.4 MB (11.62%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,7 +1,9 @@
 class Node:
     def __init__(self, key, val):
-        self.key, self.val = key, val
-        self.prev = self.next = None
+        self.key = key
+        self.val = val
+        self.next = None
+        self.prev = None
         
 class LRUCache(object):
 
@@ -14,12 +16,24 @@ class LRUCache(object):
         self.head, self.tail = Node(0, 0), Node(0, 0)
         self.head.next, self.tail.prev = self.tail, self.head
 
+    def insert(self, node):
+        p = self.tail.prev
+        p.next = node
+        node.next = self.tail
+        self.tail.prev = node
+        node.prev = p
+
+    def remove(self, node):
+        p = node.prev
+        p.next = node.next
+        node.next.prev = p
+
     def get(self, key):
         """
         :type key: int
         :rtype: int
         """
-        if (key in self.cache) == False:
+        if key not in self.cache:
             return -1
         n = self.cache[key]
         self.remove(n)
@@ -45,17 +59,8 @@ class LRUCache(object):
         self.cache[key] = n
         self.insert(n)
 
-    def insert(self, node):
-        p = self.tail.prev
-        p.next = node
-        node.prev = p
-        node.next = self.tail
-        self.tail.prev = node
 
-    def remove(self, node):
-        p = node.prev
-        p.next = node.next
-        node.next.prev = p
+
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
