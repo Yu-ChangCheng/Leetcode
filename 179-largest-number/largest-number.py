31cc04533a5b0f3e8e0dd9e88073658e76014bd9
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-04-14 22:20:50 -0500

    Time: 22 ms (66.37%) | Memory: 11.9 MB (9.03%) - LeetSync

--- 179-largest-number/largest-number.py
+++ 179-largest-number/largest-number.py
@@ -1,14 +1,12 @@
 class Solution(object):
     def largestNumber(self, nums):
-        # Convert numbers to strings to prepare for comparison
-        str_nums = list(map(str, nums))
+        nums = list(map(str,nums))
+
+        def compare(n1, n2):
+            if n1 + n2 > n2 + n1:
+                return -1
+            else:
+                return 1
         
-        # Sort numbers based on a custom key without using cmp_to_key
-        # Here, we sort by the value of concatenated pairs in reverse order
-        str_nums.sort(key=lambda x: x*10, reverse=True)
-        
-        # Concatenate into a single string
-        largest_num = ''.join(str_nums)
-        
-        # Convert to int and back to string to remove leading zeros
-        return str(int(largest_num))
+        nums = sorted(nums, key = cmp_to_key(compare))
+        return str(int("".join(nums)))
