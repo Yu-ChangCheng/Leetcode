86419606aa9d9d607824df53972bfb4bf1997063
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-04-15 16:58:09 -0500

    Time: 17 ms (27.60%) | Memory: 11.4 MB (96.19%) - LeetSync

--- 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.py
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.py
@@ -4,8 +4,11 @@ class Solution(object):
         :type num: int
         :rtype: int
         """
-        ct = 0
+        count = 0
         while num != 0:
-            num = num / 2 if num % 2 == 0 else num - 1
-            ct += 1
-        return ct
+            if (num % 2) == 0:
+                num = num // 2
+            else:
+                num = num - 1
+            count += 1
+        return count
