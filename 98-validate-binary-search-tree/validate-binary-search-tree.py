f7eaaa4960361907a70b14861d4ebb22f027c59b
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-04-16 11:14:50 -0500

    Time: 24 ms (68.95%) | Memory: 16 MB (82.82%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -13,7 +13,7 @@ class Solution(object):
         def valid(node, left, right):
             if not node:
                 return True
-            if not (left < node.val and node.val < right):
+            if not (left < node.val < right):
                 return False
             return (valid(node.left, left, node.val)
                 and valid(node.right, node.val, right))
