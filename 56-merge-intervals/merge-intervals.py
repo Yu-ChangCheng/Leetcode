1e676366c82e5b4d85109c0cddc50d54801d059d
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-04-15 19:34:36 -0500

    Time: 103 ms (48.34%) | Memory: 17.9 MB (6.18%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        intervals = sorted(intervals)
+        output = [intervals[0]]
+
+        for start, end in intervals[1:]:
+            if start <= output[-1][1]:
+                output[-1][1] = max(output[-1][1], end)
+            else:
+                output.append([start, end])
+        return output
+
+        
+            
