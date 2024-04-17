97d4d788c21933e88c5ed4d7f37bd5264b03ddcd
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-04-17 18:02:19 -0500

    Time: 182 ms (77.61%) | Memory: 12.9 MB (35.82%) - LeetSync

--- 453-minimum-moves-to-equal-array-elements/minimum-moves-to-equal-array-elements.py
+++ 453-minimum-moves-to-equal-array-elements/minimum-moves-to-equal-array-elements.py
@@ -7,10 +7,13 @@ class Solution(object):
         # Sum of all elements in the final array is going to be k * e. 
         # On each move, the sum of the original array is going to increase by k - 1. 
         # So after m moves, the sum will increase by m(k - 1). So,
-        # s + m(k - 1) = k * e ---- equation (i)
+        # sum + m(k - 1) = k * e ---- equation (i)
 
         # In this equation, e is the only unknown other than m. How can we find e? 
         # Whatever the smallest integer is in nums, plus the number of moves m, will be equal to e. 
         # This is because with each m, we only increment the value by 1. So,
         # min + m = e ---- equation (ii)
+        # sum + m * k - m = k * min + k * m (i + ii)
+        # sum - m = k * min
+        # m = sum - k * min
         return sum(nums) - (len(nums) * min(nums))
