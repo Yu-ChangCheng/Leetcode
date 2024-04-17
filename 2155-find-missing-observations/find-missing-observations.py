cc7889fe13074a0334ab86ecfb876c5ede38db68
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-04-17 12:33:34 -0500

    Time: 1098 ms (95.74%) | Memory: 15.8 MB (82.98%) - LeetSync

--- 2155-find-missing-observations/find-missing-observations.py
+++ 2155-find-missing-observations/find-missing-observations.py
@@ -7,26 +7,27 @@ class Solution(object):
         :rtype: List[int]
         """
 
-        sum_rolls = sum(rolls)
         m = len(rolls)
-        remain = mean * (m + n) - sum_rolls
+        total_required_sum = mean * (m + n)  # Total sum required to meet the mean
+        sum_rolls = sum(rolls)  # Sum of given rolls
+        remain = total_required_sum - sum_rolls  # Sum needed from missing rolls
+        
+        # If the remaining sum to find is out of possible range for n rolls, return empty
         if remain > n * 6 or remain < n:
             return []
 
+        # Start each element in the result list with 1 (minimum possible value for a roll)
+        result = [1] * n
+        remain -= n  # Adjust remain after setting each to 1 (since 1*n is already used)
         
-        res = [1] * n
-        remain -= n
-
-        # i is used as index in the res
+        # Distribute the remaining sum to make up to the mean requirement
         i = 0
         while remain > 0:
-            while remain > 5:
-                res[i] += 5
-                remain -= 5
-                i += 1
-            res[i] += remain
-            return res
-        return res
-            
+            add = min(5, remain)  # We can add up to 5 to each element (since max roll is 6)
+            result[i] += add
+            remain -= add
+            i += 1  # Move to the next roll
+
+        return result
             
         
