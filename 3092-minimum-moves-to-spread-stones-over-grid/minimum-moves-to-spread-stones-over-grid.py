4f3eaebac2944429f29a7a7a81ffbdb171d6082a
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-04-18 14:08:26 -0500

    Time: 25 ms (96.77%) | Memory: 11.6 MB (64.52%) - LeetSync

--- 3092-minimum-moves-to-spread-stones-over-grid/minimum-moves-to-spread-stones-over-grid.py
+++ 3092-minimum-moves-to-spread-stones-over-grid/minimum-moves-to-spread-stones-over-grid.py
@@ -18,23 +18,20 @@ class Solution(object):
                     zeros.append((r,c))
                 elif grid[r][c] > 1:
                     extra[(r, c)] = grid[r][c]
-        
+
         def backtracking(zero_index, extra_dict, curr_distance):
             if zero_index == len(zeros):
-                self.minimum_distance = min(self.minimum_distance, curr_distance)
-                return 
+                self.minimum_distance = min(self.minimum_distance, curr_distance)        
+                return
 
             zero_x, zero_y = zeros[zero_index]
             for (extra_x, extra_y), count in extra_dict.items():
                 if count > 1:
                     distance = abs(zero_x - extra_x) + abs(zero_y - extra_y)
                     extra_dict[(extra_x, extra_y)] -= 1
-                    backtracking(zero_index + 1, extra_dict, curr_distance + distance)
+                    backtracking(zero_index+1, extra_dict, curr_distance + distance)
                     extra_dict[(extra_x, extra_y)] += 1
             return self.minimum_distance
-            
+
         backtracking(0, extra, 0)
         return self.minimum_distance
-
-                
-        
