eb41670863c72d6c15dff51ab0d1f47eeffe9574
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-04-18 20:43:24 -0500

    Time: 23 ms (88.89%) | Memory: 12.3 MB (22.22%) - LeetSync

--- 1810-change-the-root-of-a-binary-tree/change-the-root-of-a-binary-tree.py
+++ 1810-change-the-root-of-a-binary-tree/change-the-root-of-a-binary-tree.py
@@ -14,21 +14,29 @@ class Solution(object):
         :type node: Node
         :rtype: Node
         """
-        curr = leaf
-        prev = None
+        # Node.__str__ = lambda cur: ('Node: (' +
+        #     (f'left={cur.left.val}' if cur and cur.left else 'left=None') +
+        #     (f' right={cur.right.val}' if cur and cur.right else ' right=None') +
+        #     (f' parent={cur.parent.val}' if cur and cur.parent else ' parent=None') +
+        #     ')'
+        #    )
         
-        while curr:
-            if curr.right == prev:
-                if curr == root:
-                    curr.right = None
-                    curr.parent = prev
+        cur = leaf
+        prev = None
+        while cur:
+            if cur.right == prev:
+                if cur == root:
+                    cur.right = None
+                    cur.parent = prev
+                    # print(cur)
                     break
-                elif curr.left:
-                    curr.right = curr.left
-            
-            curr.left = curr.parent
-            curr.parent = prev
-            prev = curr
-            curr = curr.left
+                elif cur.left:
+                    cur.right = cur.left
+            cur.left = cur.parent
+            cur.parent = prev
+            prev = cur
+            print(cur)
+            cur = cur.left
+        # print('')
         return leaf
             
