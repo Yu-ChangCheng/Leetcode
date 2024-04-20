a7f75915ef156c0dd54d9f8d1ac882794cf61808
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-04-20 00:07:55 -0500

    Time: 41 ms (50.33%) | Memory: 11.7 MB (48.00%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -5,19 +5,20 @@ class Solution(object):
         :type target: int
         :rtype: List[List[int]]
         """
+
         res = []
 
         def dfs(i, curr, total):
             if total == target:
-                res.append(curr[:])                
-                return 
+                res.append(curr[:])
+                return
             if total > target or i >= len(candidates):
                 return
-            
+
             curr.append(candidates[i])
             dfs(i, curr, total + candidates[i])
             curr.pop()
             dfs(i+1, curr, total)
-
         dfs(0, [], 0)
+
         return res
