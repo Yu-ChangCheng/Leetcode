7b59c61de17ce03a6a2ad2a4e37e8f6c2a4d856b
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-04-19 19:28:48 -0500

    Time: 23 ms (43.75%) | Memory: 11.7 MB (81.34%) - LeetSync

---
+++ 139-word-break/word-break.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
+        from collections import deque
+        q = deque([s])
+        seen = set()
+        while q:
+            s = q.popleft()
+            for word in wordDict:
+                if s.startswith(word):
+                    new_s = s[len(word):]
+                    if new_s == "":
+                        return True
+                    if new_s not in seen:
+                        q.append(new_s)
+                        seen.add(new_s)
+        return False
