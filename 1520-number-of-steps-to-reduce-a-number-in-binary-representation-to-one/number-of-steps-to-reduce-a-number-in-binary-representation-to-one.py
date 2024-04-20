4b6239c53b46f705545ed87e8cc3d25a162a6c6d
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-04-20 18:03:22 -0500

    Time: 17 ms (58.97%) | Memory: 11.6 MB (89.74%) - LeetSync

---
+++ 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def numSteps(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        l = len(s) - 1
+        carry = 0
+        count = 0
+        while (l > 0):
+            ## even number with carry = 0, result even
+            if int(s[l]) + carry == 0:
+                carry = 0
+                count += 1
+            ## odd number with carry = 1, result even
+            elif int(s[l]) + carry == 2:
+                carry = 1
+                count += 1
+            ## odd number with carry 
+            else:
+                carry = 1
+                count += 2
+            l -= 1
+        
+        if carry == 1:
+            count += 1
+        return count
+            
