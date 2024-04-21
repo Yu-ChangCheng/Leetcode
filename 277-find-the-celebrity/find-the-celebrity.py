5a82c8a73a1f5bc2e7be54eb352e6c384685d754
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-04-21 18:57:14 -0500

    Time: 864 ms (97.98%) | Memory: 11.9 MB (35.35%) - LeetSync

---
+++ 277-find-the-celebrity/find-the-celebrity.py
@@ -0,0 +1,23 @@
+# The knows API is already defined for you.
+# @param a, person a
+# @param b, person b
+# @return a boolean, whether a knows b
+# def knows(a, b):
+
+class Solution(object):
+    def findCelebrity(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        c = 0
+        for i in range(n):
+            if knows(c, i):
+                c = i
+
+        if any(knows(c, i) for i in range(c)):
+            return -1
+
+        if any(not knows(i, c) for i in range(n)):
+            return -1
+        return c
