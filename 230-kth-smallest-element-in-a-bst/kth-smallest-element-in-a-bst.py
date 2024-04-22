dcf59b78cbe836043c57f0067aa5faecaa97e09f
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-04-21 19:15:06 -0500

    Time: 26 ms (88.08%) | Memory: 19.3 MB (65.70%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -12,16 +12,47 @@ class Solution(object):
         :rtype: int
         """
         stack = []
-        curr = root
-
-        while stack or curr:
-            while curr:
-                stack.append(curr)
-                curr = curr.left
-            curr = stack.pop()
+        while stack or root:
+            # Traverse to the leftest node
+            while root:
+                stack.append(root)
+                root = root.left
+            # pop one and decrease k by 1
+            root = stack.pop()
             k -= 1
             if k == 0:
-                return curr.val
-                
-            curr = curr.right
-                
+                return root.val
+            # go to the right
+            root = root.right
+            
+
+    #     self.k = k
+    #     self.res = None
+    #     self.helper(root)
+    #     return self.res
+
+    # # if doing recusively, make sure no use while loop, and base case is not node
+    # # has to go to the left 
+    # def helper(self, node): 
+    #     if not node: 
+    #         return
+    #     self.helper(node.left)
+    #     self.k -= 1
+    #     if self.k == 0:
+    #         self.res = node.val
+    #         return
+    #     self.helper(node.right)
+
+
+    ''' Iterative
+    stack = []
+    while root or stack:
+        while root:
+            stack.append(root)
+            root = root.left
+        root = stack.pop()
+        k -= 1
+        if k == 0:
+            return root.val
+        root = root.right
+    '''            
