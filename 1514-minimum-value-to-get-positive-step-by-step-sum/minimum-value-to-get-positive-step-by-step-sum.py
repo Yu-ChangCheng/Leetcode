f2ffd4106149ab4f7f79785c739a7b02af49309e
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-04-24 18:27:51 -0500

    Time: 14 ms (70.48%) | Memory: 11.8 MB (15.56%) - LeetSync

--- 1514-minimum-value-to-get-positive-step-by-step-sum/minimum-value-to-get-positive-step-by-step-sum.py
+++ 1514-minimum-value-to-get-positive-step-by-step-sum/minimum-value-to-get-positive-step-by-step-sum.py
@@ -4,13 +4,22 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+
+        '''
         start_value = 1
 
         for n in nums[::-1]:
-            if start_value - n < 1:
+            if start_value - n < 1: 
                 start_value = 1
             else:  
                 start_value += -n
         return start_value
+        '''
+        prefix_sum = 0
+        min_start_value = 1
 
+        for num in nums:
+            prefix_sum += num
+            min_start_value = max(min_start_value, 1 - prefix_sum)
+        return min_start_value
         
