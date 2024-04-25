624cd06bd957fce66e7abe60076d9329f57d07b8
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-04-24 19:09:54 -0500

    Time: 53 ms (99.44%) | Memory: 14 MB (74.58%) - LeetSync

---
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def minRemoveToMakeValid(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        s = list(s)
+        stack = []
+        for i, char in enumerate(s):
+            if char == "(":
+                stack.append(i)
+            elif char == ")":
+                if stack:
+                    stack.pop()
+                else:
+                    s[i] = ""
+        while stack:
+            s[stack.pop()] = ""
+        return ''.join(s)
