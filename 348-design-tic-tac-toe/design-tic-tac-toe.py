f0f14aafd1b8ab0d4447bb947bcd7eadf75a0b4d
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-04-24 20:17:03 -0500

    Time: 48 ms (98.83%) | Memory: 14.2 MB (18.29%) - LeetSync

--- 348-design-tic-tac-toe/design-tic-tac-toe.py
+++ 348-design-tic-tac-toe/design-tic-tac-toe.py
@@ -4,10 +4,10 @@ class TicTacToe(object):
         """
         :type n: int
         """
-        self.row = [0] * n
-        self.col = [0] * n
+        self.rows = [0] * n
+        self.cols = [0] * n
         self.diag = 0
-        self.anti_diag = 0
+        self.reverse_diag = 0
         self.n = n
 
     def move(self, row, col, player):
@@ -18,15 +18,16 @@ class TicTacToe(object):
         :rtype: int
         """
         offset = 1 if player == 1 else -1
-        self.row[row] += offset
-        self.col[col] += offset
+        self.rows[row] += offset
+        self.cols[col] += offset
         if row == col:
             self.diag += offset
         if row + col == self.n - 1:
-                self.anti_diag += offset
-        if abs(self.row[row]) == self.n or abs(self.col[col]) == self.n or abs(self.diag) == self.n or abs(self.anti_diag) == self.n:
+            self.reverse_diag += offset
+        if abs(self.rows[row]) == self.n or abs(self.cols[col]) == self.n or abs(self.diag) == self.n or abs(self.reverse_diag) == self.n:
             return player
         return 0
+
 
 
 # Your TicTacToe object will be instantiated and called as such:
