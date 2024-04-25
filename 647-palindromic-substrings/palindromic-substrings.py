640ec8ace940bdbb506b142c72bb338af093ea96
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-04-25 15:12:10 -0500

    Time: 232 ms (40.52%) | Memory: 11.6 MB (76.39%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.py
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -6,15 +6,13 @@ class Solution(object):
         """
         self.res = 0
 
-        def palindrom_count(left, right):
-            while left >=0 and right < len(s) and s[left] == s[right]:
+        def isPalindrom(left, right, s):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
                 self.res += 1
                 left -= 1
                 right += 1
-
-
+        
         for i in range(len(s)):
-            palindrom_count(i, i )
-            palindrom_count(i, i + 1)
+            isPalindrom(i, i, s)
+            isPalindrom(i, i+1, s)
         return self.res
-
