a3b4ebf6168d0d331a80ef2f855b525d80520aaf
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-04-24 21:03:20 -0500

    Time: 28 ms (40.82%) | Memory: 11.8 MB (38.78%) - LeetSync

--- 179-largest-number/largest-number.py
+++ 179-largest-number/largest-number.py
@@ -1,6 +1,6 @@
 class Solution(object):
     def largestNumber(self, nums):
-        nums = list(map(str, nums))
+        nums = list(map(str, nums)) # map all elements in nums into string
 
         def compare(N1, N2):
             if N1 + N2 >= N2 + N1:
@@ -9,5 +9,5 @@ class Solution(object):
                 return 1
         
         nums = sorted(nums, key = cmp_to_key(compare))
-        if nums[0] == "0": return '0'
+        if nums[0] == "0": return "0"
         return "".join(nums)
