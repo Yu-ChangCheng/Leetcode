9f906438e59aad3fd15432c5d4609cfadf2122ea
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-04-26 16:51:30 -0500

    Time: 84 ms (24.15%) | Memory: 15.1 MB (46.65%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -5,25 +5,16 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-        # Bucket Sort
-        # freq = 
-        #[[1][2][3][4][5][6]] count
-        #[[3][2][1][0][0][0]] num
-
-        count = defaultdict(int)
-        freq = [[] for i in range(len(nums) + 1)]
-        
-        for n in nums:
-            count[n] += 1
-        for i, value in count.items():
-            freq[value].append(i)
-
+        min_heap = []
+        look_up = Counter(nums)
         res = []
-        for n in range(len(freq)-1, -1, -1):
-            for c in freq[n]:
-                res.append(c)
-                if len(res) == k:
-                    return res
 
-
-
+        for n, count in look_up.items():
+            if len(min_heap) >= k:
+                heapq.heappushpop(min_heap, (count, n))
+            else:
+                heapq.heappush(min_heap, (count, n))
+        
+        for i in range(k-1, -1, -1):
+            res.append(min_heap[i][1])
+        return res
