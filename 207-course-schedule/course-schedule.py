e1431cf5fb8fa39c6f5a7b4e7d1343478f74abb1
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-04-29 13:18:26 -0500

    Time: 64 ms (75.21%) | Memory: 14.8 MB (19.06%) - LeetSync

---
+++ 207-course-schedule/course-schedule.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
+        seen = set()
+        lookup = defaultdict(list)
+        
+        for course, pre in prerequisites:
+            lookup[course].append(pre)
+        
+        def dfs(course):
+            if course in seen:
+                return False
+            if lookup[course] == []:
+                return True
+            
+            seen.add(course)
+            for pre in lookup[course]:
+                if not dfs(pre):
+                    return False
+            seen.remove(course)
+            lookup[course] = []
+            return True
+
+
+
+        for i in range(numCourses):
+            if not dfs(i):
+                return False
+        return True
+            
+
