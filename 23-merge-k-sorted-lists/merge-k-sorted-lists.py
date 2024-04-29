bb22df585ca21cee0b3c5dc1f4cd35aca87e81b6
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-04-29 16:42:51 -0500

    Time: 82 ms (44.60%) | Memory: 17.5 MB (98.48%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -9,6 +9,7 @@ class Solution(object):
         :type lists: List[ListNode]
         :rtype: ListNode
         """
+        '''
         min_heap = []
         for idx, node in enumerate(lists):
             if node:
@@ -25,3 +26,37 @@ class Solution(object):
             if lists[idx]:
                 heappush(min_heap, (lists[idx].val, idx))
         return dummy.next
+        '''
+        if not lists or len(lists) == 0:
+            return None
+        
+        while len(lists) > 1:
+            mergedLists = []
+
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i+1] if (i+1) < len(lists) else None
+                mergedLists.append(self.mergeList(l1, l2))
+            lists = mergedLists
+        return lists[0]
+
+    def mergeList(self, l1, l2):
+        dummy = ListNode()
+        tail = dummy
+
+        while l1 and l2:
+            if l1.val < l2.val:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next= l2
+                l2 = l2.next
+            tail = tail.next
+        if l2:
+            tail.next = l2
+
+        if l1:
+            tail.next = l1
+        return dummy.next
+        
+                
