30c727853e591cbe9472469367f6fa96bcffa77a
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-04-30 00:32:59 -0500

    Time: 85 ms (21.05%) | Memory: 16.6 MB (26.32%) - LeetSync

--- 2191-minimum-number-of-food-buckets-to-feed-the-hamsters/minimum-number-of-food-buckets-to-feed-the-hamsters.py
+++ 2191-minimum-number-of-food-buckets-to-feed-the-hamsters/minimum-number-of-food-buckets-to-feed-the-hamsters.py
@@ -4,6 +4,12 @@ class Solution(object):
         :type hamsters: str
         :rtype: int
         """
+
+        # If you find a house at index i, check i-1 doesn't have a bucket. If it does, continue
+        # else: place a bucket at i+1 if you can or i-1 if you can't at i+1
+        # if you can't find a bucket or place a bucket, then return -1
+        # Time complexity: O(n), you only go through each element once
+        # Space complexity: O(n), to build the array used for DP
         hamsters = list(hamsters)
         num = 0
         for i in range(len(hamsters)):
