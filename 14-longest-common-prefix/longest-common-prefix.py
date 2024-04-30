75b3ead5bc8451f6695a2956f7862f3a145d120c
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-04-30 01:00:33 -0500

    Time: 10 ms (94.79%) | Memory: 11.8 MB (64.69%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -4,10 +4,25 @@ class Solution(object):
         :type strs: List[str]
         :rtype: str
         """
-        pre = strs[0]
+        if not strs:
+            return ""
 
-        for i in strs:
-            while not i.startswith(pre):
-                pre = pre[:-1]
-        return pre
+        if len(strs) == 1:
+            return strs[0]
+
+        commonPrefix = ""
+        for i in range(len(strs[0])):
+            char = strs[0][i]
+            for string in strs[1:]:
+                if i >= len(string) or string[i] != char:
+                    return commonPrefix
+            commonPrefix += char
+        return commonPrefix
+
+        # pre = strs[0]
+
+        # for i in strs:
+        #     while not i.startswith(pre):
+        #         pre = pre[:-1]
+        # return pre
             
