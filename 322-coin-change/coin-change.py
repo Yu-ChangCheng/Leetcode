b15bea46f31e2e91adde584b7ad382ae0ba36cbd
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-05-02 12:24:27 -0500

    Time: 750 ms (61.76%) | Memory: 12.1 MB (72.67%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -8,6 +8,13 @@ class Solution(object):
         :type amount: int
         :rtype: int
         """
+        dp = [amount+1] * (amount + 1)
+        dp[0] = 0
+        for i in range(1, amount+1):
+            for c in coins:
+                if i - c >= 0:
+                    dp[i] = min(dp[i], 1 + dp[i-c])
+        return dp[amount] if dp[amount] != amount + 1 else -1
 
         q = deque([[amount, 0]])
         visited = set()
