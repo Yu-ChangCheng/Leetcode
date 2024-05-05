02918e16ce3ada1036510adacfc5fec74f0bbe0c
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-05-05 13:22:35 -0500

    Time: 752 ms (82.48%) | Memory: 15.5 MB (19.65%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -4,19 +4,20 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        res = []
         nums.sort()
-        
+        res = []
+
         for i, a in enumerate(nums):
-            if i > 0 and a == nums[i-1]:
+            if i > 0 and nums[i] == nums[i-1]:
+                i += 1
                 continue
             l = i + 1
             r = len(nums) - 1
             while l < r:
-                three_sum = a + nums[l] + nums[r]
-                if three_sum > 0:
+                threeSum = a + nums[l] + nums[r]
+                if threeSum > 0:
                     r -= 1
-                elif three_sum < 0:
+                elif threeSum < 0:
                     l += 1
                 else:
                     res.append([a, nums[l], nums[r]])
@@ -24,3 +25,5 @@ class Solution(object):
                     while l < r and nums[l] == nums[l-1]:
                         l += 1
         return res
+                
+
