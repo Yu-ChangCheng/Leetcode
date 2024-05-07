236b056819c86157494a458a25aaaf3a31c43239
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-05-07 16:25:31 -0500

    Time: 18 ms (44.53%) | Memory: 11.6 MB (95.83%) - LeetSync

---
+++ 78-subsets/subsets.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        self.res = []
+        curr =[]
+
+        def backtracking(i, curr):
+            if i == len(nums):
+                self.res.append(curr[:])
+                return
+            
+            curr.append(nums[i])
+            backtracking(i+1, curr)
+            curr.pop()
+            backtracking(i+1, curr)
+        backtracking(0,curr)
+        return self.res
