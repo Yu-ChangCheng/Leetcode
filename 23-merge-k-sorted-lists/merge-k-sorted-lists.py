266cac7db9611a15961f363b3851fdb08b8a9226
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-05-07 12:56:01 -0500

    Time: 68 ms (81.50%) | Memory: 17.9 MB (38.73%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -9,27 +9,27 @@ class Solution(object):
         :type lists: List[ListNode]
         :rtype: ListNode
         """
-        '''
         min_heap = []
         for idx, node in enumerate(lists):
             if node:
                 heappush(min_heap, (node.val, idx))
-
+        
         head = ListNode()
         dummy = head
 
         while min_heap:
             val, idx = heappop(min_heap)
-            head.next = lists[idx]
+            head.next =lists[idx]
             head = head.next
             lists[idx] = lists[idx].next
             if lists[idx]:
-                heappush(min_heap, (lists[idx].val, idx))
+                heappush(min_heap,(lists[idx].val, idx))
         return dummy.next
-        '''
+
         if not lists or len(lists) == 0:
             return None
         
+        '''
         while len(lists) > 1:
             mergedLists = []
 
@@ -58,5 +58,5 @@ class Solution(object):
         if l1:
             tail.next = l1
         return dummy.next
-        
+        '''
                 
