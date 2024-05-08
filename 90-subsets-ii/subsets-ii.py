015524bcc93e365e67811977e84dc6da2291e3bb
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-05-07 21:00:37 -0500

    Time: 17 ms (70.00%) | Memory: 11.8 MB (82.96%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -4,17 +4,22 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        self.res = set()
+        nums.sort()
+        self.res = []
         curr =[]
 
         def backtracking(i, curr):
             if i == len(nums):
-                self.res.add(tuple(sorted(curr[:])))
+                self.res.append(curr[:])
                 return
             
             curr.append(nums[i])
             backtracking(i+1, curr)
             curr.pop()
+
+            while i + 1 < len(nums) and nums[i] == nums[i+1]:
+                i += 1
             backtracking(i+1, curr)
+
         backtracking(0,curr)
         return self.res
