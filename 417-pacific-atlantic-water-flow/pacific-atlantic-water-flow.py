658ad2ab873165bc6d8bb1c6774138d2e8088413
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-05-09 14:08:08 -0500

    Time: 199 ms (89.59%) | Memory: 13.6 MB (60.49%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -10,8 +10,8 @@ class Solution(object):
         alt_set = set()
 
         def dfs(r, c, visited_set, prev_val):
-            if ((r,c) in visited_set or r < 0 or c < 0 or r == rows  or c == cols or heights[r][c] < prev_val):
-                return  
+            if ((r,c) in visited_set or r < 0 or c < 0 or r > rows-1 or c > cols-1 or heights[r][c] < prev_val):
+                return
             visited_set.add((r,c))
             dfs(r+1, c, visited_set, heights[r][c])
             dfs(r-1, c, visited_set, heights[r][c])
@@ -21,14 +21,12 @@ class Solution(object):
         for c in range(cols):
             dfs(0, c, pac_set, heights[0][c])
             dfs(rows-1, c, alt_set, heights[rows-1][c])
-        
+
         for r in range(rows):
-            dfs(r, 0 , pac_set,heights[r][0])
-            dfs(r, cols-1, alt_set, heights[r][cols-1])
-
+            dfs(r, 0, pac_set, heights[r][0])
+            dfs(r,cols-1, alt_set, heights[r][cols-1])
         res = []
-        for (pac_x, pac_y) in pac_set:
-            if (pac_x, pac_y) in alt_set:
-                res.append([pac_x, pac_y])
-
+        for (r,c) in pac_set:
+            if (r,c) in alt_set:
+                res.append((r,c))
         return res
