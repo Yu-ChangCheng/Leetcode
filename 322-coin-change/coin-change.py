b2c3b42eb533c0743585630f6d76f1de1aa859da
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-05-10 16:26:55 -0500

    Time: 386 ms (98.00%) | Memory: 12.6 MB (26.43%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -8,6 +8,22 @@ class Solution(object):
         :type amount: int
         :rtype: int
         """
+
+
+        q = deque()
+        q.append([amount,0])
+        visited = set()
+        while q:
+            new_cur, n_coins = q.popleft()
+            if new_cur == 0:
+                return n_coins
+            for c in coins:
+                reminder = new_cur - c
+                if reminder not in visited and reminder >= 0:
+                    q.append([reminder, 1 + n_coins])
+                    visited.add(reminder)
+        return -1 
+        
         dp = [amount+1] * (amount + 1)
         dp[0] = 0
         for i in range(1, amount+1):
@@ -15,21 +31,3 @@ class Solution(object):
                 if i - c >= 0:
                     dp[i] = min(dp[i], 1 + dp[i-c])
         return dp[amount] if dp[amount] != amount + 1 else -1
-
-        q = deque([[amount, 0]])
-        visited = set()
-
-        while q:
-            new_cur, n_coins = q.popleft()
-
-            if new_cur == 0:
-                return n_coins
-         
-            for c in coins:
-                reminder = new_cur - c
-                if reminder not in visited and reminder >= 0:
-                    q.append([reminder, 1 + n_coins])
-                    visited.add(reminder) 
-
-        return -1
-
