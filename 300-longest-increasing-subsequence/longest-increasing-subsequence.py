ee1b0458353077924f3a5ee093309b20cc0b8eb0
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-05-10 17:20:23 -0500

    Time: 2155 ms (15.10%) | Memory: 12.1 MB (35.55%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -6,7 +6,10 @@ class Solution(object):
         """
         LIS = [1] * len(nums)
         for i in range(len(nums)-1, -1, -1):
-            for j in range(i, len(nums)):
-                if nums[j] - nums[i] > 0:
-                    LIS[i] = max(LIS[i], LIS[j] + 1)
+            for j in range(i+1,len(nums)):
+                if nums[i] < nums[j]:
+                    LIS[i] = max(LIS[i], 1+LIS[j])
         return max(LIS)
+
+        # O(N^2)
+        # O(N)
