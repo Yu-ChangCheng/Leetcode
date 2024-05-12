99b21b34855622a82f317c4b65ff0d53d8ca2e53
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-05-12 15:14:23 -0500

    Time: 81 ms (72.47%) | Memory: 13.4 MB (40.02%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -4,22 +4,27 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
-        if not height: return 0
-
-        l = 0
-        r = len(height) -1 
-        res = 0
-        leftMax = height[l]
-        rightMax = height[r]
-
-        while l < r:
-            if leftMax < rightMax:
-                l += 1
-                res += max(leftMax - height[l], 0)
-                leftMax = max(leftMax, height[l])
+        water = 0
+        leftMax = 0
+        rightMax = 0
+        # find the highest index in the middle
+        highest_index = 0
+        for i in range(len(height)):
+            if height[i] > height[highest_index]:
+                highest_index = i
+        
+        # count the water from left
+        for i in range(highest_index):
+            if height[i] > leftMax:
+                leftMax = height[i]
             else:
-                r -= 1
-                res += max(rightMax - height[r], 0)
-                rightMax = max(rightMax, height[r])
-                
-        return res
+                water += leftMax - height[i]
+
+        # count the water from right
+        for i in range(len(height)-1, highest_index, -1):
+            if height[i] > rightMax:
+                rightMax = height[i]
+            else:
+                water += rightMax - height[i]
+        return water
+            
