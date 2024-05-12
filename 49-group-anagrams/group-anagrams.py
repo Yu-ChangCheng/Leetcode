efc14c4746ca9b62785588f25138890acbb88e5b
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-05-12 14:16:53 -0500

    Time: 83 ms (25.49%) | Memory: 18 MB (7.71%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -4,7 +4,14 @@ class Solution(object):
         :type strs: List[str]
         :rtype: List[List[str]]
         """
-        
+        res = defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            res[tuple(count)].append(s)
+        return res.values()
+
         res = defaultdict(list)
         for s in strs:
             count = [0] * 26
