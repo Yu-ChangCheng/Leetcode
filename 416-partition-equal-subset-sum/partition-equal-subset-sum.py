e524c1f934c816ccd516286be7ee1da30464fdf7
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-05-15 16:19:16 -0500

    Time: 768 ms (69.15%) | Memory: 13.9 MB (44.65%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -4,15 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
+        # if it is odd then return false
         if sum(nums) % 2 != 0:
             return False
         
         dp = set()
         target = sum(nums) // 2
-        dp.add(0)
+        dp.add(0) # have 0 as intial value
 
         for i in range(len(nums)):
             for t in dp.copy():
-                dp.add(t)
                 dp.add(nums[i]+t)
         return True if target in dp else False
