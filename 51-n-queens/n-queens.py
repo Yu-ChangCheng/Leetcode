d28d4b96cb067c6c4931881a4f125e1b1211944b
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-05-15 11:10:17 -0500

    Time: 14 ms (99.85%) | Memory: 12.1 MB (30.32%) - LeetSync

--- 51-n-queens/n-queens.py
+++ 51-n-queens/n-queens.py
@@ -8,28 +8,32 @@ class Solution(object):
         pos_diag_set = set() # (r-c) as r - c will be the key for the diag -3, -2, -1,  0,  1,  2,  3
         neg_diag_set = set() # (r+c) as r + c will be the key for the diag  0,  1,  2,  3,  4,  5,  6
         res = []
-        board = [["."] * n for r in range(n)]
+        board = [["."] * n for r in range(n)] # 
 
         def backtracking(r):
+            # base case
             if r == n:
                 temp = []
                 for r in range(n):
                     row_string = "".join(board[r])
                     temp.append(row_string)
                 res.append(temp)
-                return
-            
+
+            # iterate column for rows 
             for c in range(n):
                 if c in col_set or (r-c) in pos_diag_set or (r+c) in neg_diag_set:
                     continue
-                
+
+                # add to the set for the current row
                 col_set.add(c)
                 pos_diag_set.add(r-c)
                 neg_diag_set.add(r+c)
                 board[r][c] = "Q"
+                
+                # iterate thorugh every row
+                backtracking(r+1)
 
-                backtracking(r + 1)
-
+                # remove r, and c from the board
                 col_set.remove(c)
                 pos_diag_set.remove(r-c)
                 neg_diag_set.remove(r+c)
