339e531febfd3513f9f659799d98c48ed114caa6
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-05-16 13:19:52 -0500

    Time: 100 ms (78.72%) | Memory: 14 MB (19.15%) - LeetSync

--- 2475-largest-palindromic-number/largest-palindromic-number.py
+++ 2475-largest-palindromic-number/largest-palindromic-number.py
@@ -4,6 +4,24 @@ class Solution(object):
         :type num: str
         :rtype: str
         """
+        counts = Counter(num)
+        list_counts = sorted([[value, count] for value, count in counts.items()], key = lambda x: x[0])[::-1]
+        left = ""
+        mid = ""
+
+        for value, count in list_counts:
+            curr_c = value * (count // 2)
+            if count % 2 == 0:
+                left += curr_c
+            else:
+                left += curr_c
+                mid = max(mid, value)
+        left = left.lstrip('0')
+        if left + mid + left[::-1]:
+            return left + mid + left[::-1]
+        else:
+            return "0"
+        
         counts = Counter(num) # {"9": 3}
         list_counts = sorted([[value, count] for value, count in counts.items()], key = lambda x: x[0])[::-1]
         left = ""
