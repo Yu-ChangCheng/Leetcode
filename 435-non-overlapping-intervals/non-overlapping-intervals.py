657fb4f471b9f3dee5af4eac01b583879cdaa4cd
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-05-15 19:02:28 -0500

    Time: 1132 ms (31.07%) | Memory: 58 MB (54.40%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.py
+++ 435-non-overlapping-intervals/non-overlapping-intervals.py
@@ -8,6 +8,19 @@ class Solution(object):
         intervals.sort()
         last_end = intervals[0][1]
 
+        res = 0
+        intervals.sort()
+        last_end = intervals[0][1]
+        for i in range(1,len(intervals)):
+            next_start = intervals[i][0]
+            next_end = intervals[i][1]
+            if next_start < last_end:
+                res += 1
+                last_end = min(last_end, next_end)
+            else:
+                last_end = next_end
+        return res
+        
         for i in range(1, len(intervals)):
             next_start = intervals[i][0]
             next_end = intervals[i][1]
