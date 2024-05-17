0b16821e23acb00d8a60437f0361fe5b3ddbc912
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-05-17 11:46:48 -0500

    Time: 14 ms (99.31%) | Memory: 12.2 MB (67.16%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -9,6 +9,20 @@ class Solution(object):
             return False
         
         s_dict = defaultdict(int)
+        for c in s: 
+            s_dict[c] += 1
+        
+        for c in t:
+            if c not in s_dict or s_dict[c] <= 0:
+                return False
+            else:
+                s_dict[c] -= 1
+        return True
+        
+        if len(s) != len(t):
+            return False
+        
+        s_dict = defaultdict(int)
         for c in s:
             s_dict[c] += 1
         
