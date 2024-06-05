1e4786b54fd8f76a6afaaeb5bd56f44f8074d70d
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-06-05 11:28:30 -0500

    Time: 56 ms (97.62%) | Memory: 14.8 MB (20.48%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -18,6 +18,31 @@ class Solution(object):
                 return True
             seen.add(course)
             for pre in lookup[course]:
+                if dfs(pre) == False:
+                    return False
+            seen.remove(course)
+            lookup[course] = []
+            return True
+        
+        for i in range(numCourses):
+            if dfs(i) == False:
+                return False
+        return True
+
+
+        seen = set()
+        lookup = defaultdict(list)
+
+        for course, pre in prerequisites:
+            lookup[course].append(pre)
+        
+        def dfs(course):
+            if course in seen:
+                return False
+            if lookup[course] == []:
+                return True
+            seen.add(course)
+            for pre in lookup[course]:
                 if not dfs(pre):
                     return False
             seen.remove(course)
