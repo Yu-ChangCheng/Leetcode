1bda1550d5535abc4a398b6c4c3bb601f5d97d42
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-06-07 14:00:35 -0500

    Time: 41 ms (20.69%) | Memory: 24.6 MB (65.91%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.py
@@ -15,6 +15,20 @@ class Solution(object):
         def depth(node):
             if not node:
                 return 0
+            
+            left = depth(node.left)
+            right = depth(node.right)
+            self.diameter = max(self.diameter, left + right)
+            return 1 + max(left, right)
+
+        depth(root)
+        return self.diameter
+
+        self.diameter = 0
+
+        def depth(node):
+            if not node:
+                return 0
 
             left = depth(node.left)
             right = depth(node.right)
