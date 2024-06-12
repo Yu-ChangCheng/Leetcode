62ecf71f6b13cf686fc3e5060af19cfe19af140f
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-06-12 13:16:39 -0500

    Time: 12 ms (92.28%) | Memory: 13.4 MB (91.19%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.py
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -13,8 +13,19 @@ class Solution(object):
         curr = head
 
         while curr:
-            temp = curr.next #(2) (3)
+            next_node = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_node
+        
+        return prev
+
+        prev = None
+        curr = head
+
+        while curr:
+            next_node = curr.next #(2) (3)
             curr.next = prev #(->None) (->1)
             prev = curr #(None to 1) (2)
-            curr = temp #(2) (3)
+            curr = next_node #(2) (3)
         return prev
