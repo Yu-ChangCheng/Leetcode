6630dfbad90e3c72dda70015cbb45722f82d5f98
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-07-10 11:00:44 -0500

    Time: 35 ms (86.06%) | Memory: 12.5 MB (35.72%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -14,3 +14,7 @@ class Solution(object):
             else:
                 hashmap[complement] = i
         
+        # Time: O(n) since it is a linear scan for the entire array
+        # Space: O(n) since we have a hashmap to store the index
+
+        
