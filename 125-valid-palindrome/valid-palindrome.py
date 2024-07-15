d702b664a1b84ac2e17490d8a3103561790e2807
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-07-15 18:55:38 -0500

    Time: 39 ms (38.55%) | Memory: 12.4 MB (77.85%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -16,17 +16,3 @@ class Solution(object):
             l += 1
             r -= 1
         return True
-
-        l = 0 
-        r = len(s) - 1
-
-        while l < r:
-            while l < r and not s[l].isalnum():
-                l += 1
-            while l < r and not s[r].isalnum():
-                r -= 1
-            if l < r and s[l].lower() != s[r].lower():
-                return False
-            l += 1
-            r -= 1
-        return True
