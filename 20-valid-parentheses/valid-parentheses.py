bfbfb4c9e563a1ba911b5d70d2b68c786e2991e6
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-07-23 13:50:32 -0500

    Time: 21 ms (17.56%) | Memory: 11.7 MB (75.56%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -4,15 +4,15 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        bracket_map = {")":"(",    "}":"{",   "]":"["}
+        bracket_map = {")":"(", "}":"{",  "]":"["}
         stack = []
         for c in s:
             if c not in bracket_map:
                 stack.append(c)
                 continue
-            elif stack != [] and bracket_map.get(c) == stack[-1]:
+            elif stack and bracket_map[c] == stack[-1]:
                 stack.pop()
             else:
                 return False
-        if stack != []: return False
+        if stack: return False
         return True
