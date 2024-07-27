a6cb52042b63f4442effadaa854e74d0543c32f0
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-07-27 15:51:01 -0500

    Time: 14 ms (89.92%) | Memory: 11.8 MB (18.01%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -10,8 +10,9 @@ class Solution(object):
         :type list2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
+        
         tail = ListNode() # dummy is None return dummy.next
-        dummy = tail
+        dummy = tail # shallow copy
 
         while list1 and list2:
             if list1.val < list2.val:
@@ -22,7 +23,7 @@ class Solution(object):
                 list2 = list2.next
             tail = tail.next # update the current pointer
 
-        if list1:
+        if list1: # if any list still exist, append to the linked list
             tail.next = list1
         if list2:
             tail.next = list2
