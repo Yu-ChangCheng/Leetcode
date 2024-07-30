c262fcdca3aff8fd59dca1c10e1a1a443237b868
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-07-29 19:29:13 -0500

    Time: 808 ms (11.93%) | Memory: 21 MB (9.94%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -4,30 +4,18 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
+        # [7,2,3,1,5,3,6,9]
+
         l = 0
         r = 0
         maxProfit = 0
-        while r < len(prices) and l <= r:
-            if prices[r] > prices[l]:
+        while r < len(prices):
+            if prices[r] >= prices[l]: # if the right pointer is larger than the left, meaning valid window, move right pointer
                 maxProfit = max(prices[r] - prices[l], maxProfit)
-                r += 1
-            else:
+                r += 1 # 
+            else: # else the window is not valid, move the left pointer
                 l = r
-                r += 1
+                r = r + 1
         return maxProfit
 
-
-        l = 0
-        r = 0
-        maxProfit = 0
         
-        while r < len(prices) and l <= r:
-            if prices[r] > prices[l]:
-                maxProfit = max(prices[r] - prices[l], maxProfit)
-                r += 1
-            else:
-                l = r
-                r = l + 1
-
-        return maxProfit
-            
