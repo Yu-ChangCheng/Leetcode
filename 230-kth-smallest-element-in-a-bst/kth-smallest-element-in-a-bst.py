b0c68d9e0e22417ac0b94a65d2eebdabf91c63c4
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-08-08 14:59:50 -0500

    Time: 33 ms (54.55%) | Memory: 19.5 MB (21.08%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -11,20 +11,38 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        if not root:
-            return None
-        stack = []
-        stack.append(root)
+        # DFS, In-order(Left, Root, Right), recursive call
+        self.k = k
+        self.res = None
+        self.helper(root)
+        return self.res
+
+    def helper(self, node):
+        if not node:
+            return
+        self.helper(node.left) # traverse left first and when popback return the value
+        self.k -= 1 # traverse root
+        if self.k == 0: 
+            self.res = node.val 
+            return
+        self.helper(node.right) # traverse right
+
+
+
+        # # Traverse the tree In-order
+        # if not root:
+        #     return None
+        # stack = []
+        # stack.append(root)
         
-        while stack:
+        # while stack:
+        #     while root:
+        #         stack.append(root)
+        #         root = root.left
 
-            while root:
-                stack.append(root)
-                root = root.left
-
-            root = stack.pop()
-            k -= 1
-            if k == 0:
-                return root.val
-            root = root.right
+        #     root = stack.pop()
+        #     k -= 1
+        #     if k == 0:
+        #         return root.val
+        #     root = root.right
 
