da8be63183ca050bd0fda51a1002290497719eb4
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-08-13 13:07:52 -0500

    Time: 49 ms (25.39%) | Memory: 11.8 MB (13.46%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -5,20 +5,23 @@ class Solution(object):
         :type target: int
         :rtype: List[List[int]]
         """
+
         self.res = []
         curr = []
-
+        
         def backtracking(i, curr, currSum):
             if i > len(candidates) - 1 or currSum > target:
                 return
             if currSum == target:
                 self.res.append(curr[:])
                 return
-            curr.append(candidates[i])
-            backtracking(i, curr, currSum + candidates[i])
-            curr.pop()
-            backtracking(i+1, curr, currSum)
-        
+            curr.append(candidates[i]) # include the current candidates
+            backtracking(i, curr, currSum + candidates[i]) # First backtracing
+            curr.pop() # Exclud the current candidates
+            backtracking(i+1, curr, currSum) # Second backtracing
         backtracking(0, curr, 0)
         return self.res
-        
+
+        # TC: O(number of candidates^max depth), where max depth(h) ~= target val / smallest val 
+        # b/c max depth of the tree is when it keeps adding the smallest element to the combination until it hits or exceeds target. 
+        # SC: O(target val / smallest val in candidates) 
