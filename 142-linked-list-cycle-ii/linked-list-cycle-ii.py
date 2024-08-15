7b1e214328a42888fd6e4a22017fa0504714ab03
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-08-15 15:53:53 -0500

    Time: 33 ms (39.17%) | Memory: 17.9 MB (60.08%) - LeetSync

---
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.py
@@ -0,0 +1,34 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def detectCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        slow = head
+        fast = head
+        
+        # find the cycle and the point where slow meets fast
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                break
+        
+        if not fast or not fast.next:
+            return None
+
+        # start from begining 
+        slow = head
+        while fast:
+            if slow == fast:
+                return slow
+            slow = slow.next
+            fast = fast.next
+        return None
+        
