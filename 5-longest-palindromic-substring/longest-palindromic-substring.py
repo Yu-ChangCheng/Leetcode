da82b1f2adcee913b72a3b9762a0c97548895f92
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-08-28 16:58:08 -0500

    Time: 479 ms (44.00%) | Memory: 11.8 MB (66.75%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        # also check out 647
+        # The 
+        self.res = ""
+        self.res_len = 0
+
+        def isPalindrome(left, right):
+            # if in bound and equal keep expanding the window
+            while 0 <= left and right < len(s) and left <= right and s[left] == s[right]:
+                if right - left + 1 > self.res_len:
+                    self.res = s[left:right+1]
+                    self.res_len = right - left + 1
+                left -= 1
+                right += 1
+
+        # for each chars in the string TC: O(n^2) / SC: O(n)
+        for i in range(len(s)):
+            isPalindrome(i, i+1)
+            isPalindrome(i, i)
+        return self.res            
