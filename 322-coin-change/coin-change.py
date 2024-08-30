aae439e2e2cd367aaa93f71c6355a8eda8c9f14c
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-08-30 12:58:39 -0500

    Time: 749 ms (51.69%) | Memory: 12.1 MB (67.00%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -8,33 +8,33 @@ class Solution(object):
         :type amount: int
         :rtype: int
         """
-        q = deque()
-        q.append([amount,0])
-        visited = set()
-        while q:
-            new_cur, n_coins = q.popleft()
-            if new_cur == 0:
-                return n_coins
-            for c in coins:
-                reminder = new_cur - c
-                if reminder not in visited and reminder >= 0:
-                    q.append([reminder, 1 + n_coins])
-                    visited.add(reminder)
-        return -1
+        # q = deque()
+        # q.append([amount,0])
+        # visited = set()
+        # while q:
+        #     new_cur, n_coins = q.popleft()
+        #     if new_cur == 0:
+        #         return n_coins
+        #     for c in coins:
+        #         reminder = new_cur - c
+        #         if reminder not in visited and reminder >= 0:
+        #             q.append([reminder, 1 + n_coins])
+        #             visited.add(reminder)
+        # return -1
 
-        q = deque()
-        q.append([amount,0])
-        visited = set()
-        while q:
-            new_cur, n_coins = q.popleft()
-            if new_cur == 0:
-                return n_coins
-            for c in coins:
-                reminder = new_cur - c
-                if reminder not in visited and reminder >= 0:
-                    q.append([reminder, 1 + n_coins])
-                    visited.add(reminder)
-        return -1 
+        # q = deque()
+        # q.append([amount,0])
+        # visited = set()
+        # while q:
+        #     new_cur, n_coins = q.popleft()
+        #     if new_cur == 0:
+        #         return n_coins
+        #     for c in coins:
+        #         reminder = new_cur - c
+        #         if reminder not in visited and reminder >= 0:
+        #             q.append([reminder, 1 + n_coins])
+        #             visited.add(reminder)
+        # return -1 
         
         dp = [amount+1] * (amount + 1)
         dp[0] = 0
