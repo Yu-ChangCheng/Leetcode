f1e6e3ebdee0287875b9dcc22603b1af1b0d2be8
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-09-04 13:38:49 -0500

    Time: 12 ms (76.56%) | Memory: 11.5 MB (74.33%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -5,11 +5,23 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        # with 2D DP array O(m * n)
         dp = [[0] * (n + 1) for r in range(m+1)] 
         dp[m-1][n] = 1
-        print(dp)
-
+        
         for r in range(m-1, -1 , -1):
             for c in range(n-1, -1, -1):
                 dp[r][c] = dp[r+1][c] + dp[r][c+1]
         return dp[0][0]
+
+
+        
+        # with 1D DP array only O(n)
+        rows = [1] * n
+
+        for i in range(m-1):
+            new_rows = [1] * n
+            for j in range(n-2, -1, -1):   
+                new_rows[j] = new_rows[j + 1] + rows[j] # add right and below
+            rows = new_rows
+        return rows[0]
