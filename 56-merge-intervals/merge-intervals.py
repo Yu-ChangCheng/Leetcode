5bc7c8da4e80a4efaa76c9c4c2bb01f3810e3383
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-09-08 16:16:51 -0500

    Time: 112 ms (13.59%) | Memory: 16.5 MB (91.11%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -5,13 +5,13 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         intervals.sort()
-        new_interval = intervals[0]
+        curr_interval = intervals[0]
         res = []
         for i in range(1,len(intervals)):
-            if new_interval[1] < intervals[i][0]:
-                res.append(new_interval)
-                new_interval = intervals[i]
+            if curr_interval[1] < intervals[i][0]:
+                res.append(curr_interval)
+                curr_interval = intervals[i]
             else:
-                new_interval = (min(intervals[i][0], new_interval[0]), max(intervals[i][1], new_interval[1]))
-        res.append(new_interval)
+                curr_interval = (min(intervals[i][0], curr_interval[0]), max(intervals[i][1], curr_interval[1]))
+        res.append(curr_interval)
         return res
