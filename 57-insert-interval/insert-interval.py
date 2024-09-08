f27f5e150f4df3a9871699c5f915d7e734fc6f4f
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-09-08 15:42:33 -0500

    Time: 56 ms (29.95%) | Memory: 15.2 MB (33.00%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -6,47 +6,9 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         res = []
-        new_start, new_end = newInterval
-        merged = False
-        #
-        for i, (start, end) in enumerate(intervals):
-        # [1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
-            if end < new_start: # the current interval is before the newInterval
-                res.append([start, end]) # append([1,2])
-            elif start > new_end: # the current interval is after the newInterval
-                res.append([new_start, new_end])
-                merged = True
-                return res+intervals[i:]
-            else:
-                new_start, new_end = min(start, new_start), max(end, new_end)
-        
-        if not merged:
-            res.append([new_start, new_end])
-        return res
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        res = []
+        if not intervals:
+            return [newInterval]
 
         for i in range(len(intervals)):
             # if new_intervel ends before the start just append it and the rest 
@@ -56,7 +18,9 @@ class Solution(object):
             # if new_intervel start after the end just append the interval in to res
             elif newInterval[0] > intervals[i][1]:
                 res.append(intervals[i])
+            # we keep updating newIntervals until we finished the merge
             else:
                 newInterval = [min(newInterval[0],intervals[i][0]), max(newInterval[1],intervals[i][1])]
+
         res.append(newInterval)
         return res
