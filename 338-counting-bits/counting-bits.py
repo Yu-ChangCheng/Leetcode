793310f456c33dbbbc1809ce700701b0ec268d8d
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-09-18 17:01:31 -0500

    Time: 51 ms (57.17%) | Memory: 16.1 MB (5.17%) - LeetSync

--- 338-counting-bits/counting-bits.py
+++ 338-counting-bits/counting-bits.py
@@ -4,6 +4,16 @@ class Solution(object):
         :type n: int
         :rtype: List[int]
         """
+        dp = [0] * (n + 1)
+        offset = 1
+        for i in range(1,n+1):
+            if i == offset*2:
+                offset = i
+            dp[i] = 1 + dp[i - offset]
+        return dp
+            
+
+
         res = []
         for i in range(n+1):
             count = bin(i).count('1')
