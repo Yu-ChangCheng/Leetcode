bb24d05e5c0765bc36cfbfb51fe72eb038c1524f
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-09-20 14:49:18 -0500

    Time: 46 ms (31.42%) | Memory: 11.7 MB (40.91%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -6,22 +6,75 @@ class Solution(object):
         :rtype: List[List[int]]
         """
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         self.res = []
-        curr = []
-        
+        curr =[]
         def backtracking(i, curr, currSum):
-            if i > len(candidates) - 1 or currSum > target:
+            # print("i", i, "curr", curr, "currSum", currSum)
+            if i + 1 > len(candidates) or currSum > target:
                 return
             if currSum == target:
                 self.res.append(curr[:])
                 return
-            curr.append(candidates[i]) # include the current candidates
-            backtracking(i, curr, currSum + candidates[i]) # First backtracing
-            curr.pop() # Exclud the current candidates
-            backtracking(i+1, curr, currSum) # Second backtracing
+            curr.append(candidates[i])
+            backtracking(i, curr, currSum + candidates[i])
+            curr.pop()
+            backtracking(i + 1, curr, currSum)
+            
         backtracking(0, curr, 0)
         return self.res
 
-        # TC: O(number of candidates^max depth), where max depth(h) ~= target val / smallest val 
+
+
+        # self.res = []
+        # curr = []
+        
+        # def backtracking(i, curr, currSum):
+        #     if i > len(candidates) - 1 or currSum > target: # can't find the desired path
+        #         return
+        #     if currSum == target: # if find target append to the res
+        #         self.res.append(curr[:])
+        #         return
+        #     curr.append(candidates[i]) # include the current candidates
+        #     backtracking(i, curr, currSum + candidates[i]) # First backtracing
+        #     curr.pop() # Exclud the current candidates
+        #     backtracking(i+1, curr, currSum) # Second backtracing
+        # backtracking(0, curr, 0)
+        # return self.res
+
+        # TC: O(number of candidates^max depth) -> O(n^(t/min(candidates))), where max depth = h ~= target val / smallest val 
         # b/c max depth of the tree is when it keeps adding the smallest element to the combination until it hits or exceeds target. 
-        # SC: O(target val / smallest val in candidates) 
+        # SC: O(target val / smallest val in candidates) -> O(h), where max depth = h ~= target val / smallest val 
