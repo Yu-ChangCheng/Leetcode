365430f8412cf12f26137e7ee2273dc23ac8d030
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-09-23 07:51:35 -0500

    Time: 181 ms (16.78%) | Memory: 13.1 MB (97.11%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -13,6 +13,30 @@ class Solution(object):
             if height[i] > height[highest_index]:
                 highest_index = i
         
+        # count the water from left 
+        for i in range(highest_index):
+            if height[i] > leftMax:
+                leftMax = height[i]
+            else:
+                water += leftMax - height[i]
+        
+        # count the water from right
+        for i in range(len(height)-1, highest_index, -1):
+            if height[i] > rightMax:
+                rightMax = height[i]
+            else:
+                water += rightMax - height[i]
+        return water
+
+        water = 0
+        leftMax = 0
+        rightMax = 0
+        # find the highest index in the middle
+        highest_index = 0
+        for i in range(len(height)):
+            if height[i] > height[highest_index]:
+                highest_index = i
+        
         # count the water from left
         for i in range(highest_index):
             if height[i] > leftMax:
