9e77ff7622f612031bb696fc3b8bf082ba7c163e
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-10-01 14:05:57 -0500

    Time: 782 ms (38.14%) | Memory: 32.7 MB (5.24%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -4,31 +4,48 @@ class Solution(object):
         :type heights: List[int]
         :rtype: int
         """
-        heights.append(0)  # Append a zero to ensure all elements are processed
-        stack = [-1]  # Initialize stack with -1 as a dummy index
-        max_area = 0  # This will store the maximum area
+        # heights.append(0)  # Append a zero to ensure all elements are processed
+        # stack = [-1]  # Initialize stack with -1 as a dummy index
+        # max_area = 0  # This will store the maximum area
 
-        for i in range(len(heights)):
-            # While the current height is less than the height of the bar at the top of the stack
-            while heights[i] < heights[stack[-1]]:
-                h = heights[stack.pop()]  # Pop the stack and get the height
-                w = i - stack[-1] - 1  # Calculate the width using the current index and top of the stack
-                max_area = max(max_area, h * w)  # Update the maximum area
-            stack.append(i)  # Push the current index onto the stack
+        # for i in range(len(heights)):
+        #     # While the current height is less than the height of the bar at the top of the stack
+        #     while heights[i] < heights[stack[-1]]:
+        #         h = heights[stack.pop()]  # Pop the stack and get the height
+        #         w = i - stack[-1] - 1  # Calculate the width using the current index and top of the stack
+        #         max_area = max(max_area, h * w)  # Update the maximum area
+        #     stack.append(i)  # Push the current index onto the stack
 
-        return max_area  # Return the maximum area
+        # return max_area  # Return the maximum area
 
 
-        stack = [] #[index, value]
-        heights.append(0)
+        # add a 0 heights in the end to ensure all the stack got poped in the end
+        heights.append(0) 
+        stack = [] # [start_index, value]
         maxArea = 0
 
         for curr_index, curr_val in enumerate(heights):
             start = curr_index
             while stack and curr_val < stack[-1][1]:
                 index, height = stack.pop()
-                maxArea = max(maxArea, height * (curr_index - index))
-                start = index
+                if stack:
+                    width = curr_index - stack[-1][0] - 1
+                elif not stack:
+                    width = curr_index
+                maxArea = max(maxArea, height * width)
+                # start = index # update the start index
             stack.append([start, curr_val])
         
         return maxArea
+
+
+
+
+
+
+
+
+
+
+
+
