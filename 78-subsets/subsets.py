f0b50c4afbc4df625cdfec0cf4343ce418a381b7
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2024-10-10 15:40:11 -0500

    Time: 20 ms (24.33%) | Memory: 11.7 MB (69.15%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -4,42 +4,38 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        self.res = []
-        curr = []
-        def backtracking(i, curr):
-            if i == len(nums):
-                self.res.append(curr[:])
-                return
-            curr.append(nums[i])
-            backtracking(i+1, curr)
-            curr.pop()
-            backtracking(i+1, curr)
-        backtracking(0, curr)
-        return self.res
+        # self.res = []
+        # curr = []
+        # def backtracking(i, curr):
+        #     if i == len(nums):
+        #         self.res.append(curr[:])
+        #         return
+        #     curr.append(nums[i])
+        #     backtracking(i+1, curr)
+        #     curr.pop()
+        #     backtracking(i+1, curr)
+        # backtracking(0, curr)
+        # return self.res
 
-        self.res = []
-        curr =[]
+        # self.res = []
+        # curr =[]
 
-        def backtracking(i, curr):
-            if i == len(nums):
-                self.res.append(curr[:])
-                return
+        # def backtracking(i, curr):
+        #     if i == len(nums):
+        #         self.res.append(curr[:])
+        #         return
             
-            curr.append(nums[i])
-            backtracking(i+1, curr)
-            curr.pop()
-            backtracking(i+1, curr)
+        #     curr.append(nums[i])
+        #     backtracking(i+1, curr)
+        #     curr.pop()
+        #     backtracking(i+1, curr)
         
-        backtracking(0,curr)
-        print(self.res)
-        return self.res
-
-
-
-
+        # backtracking(0,curr)
+        # print(self.res)
+        # return self.res
 
         res = [[]]
-        nums.sort()
+        # nums.sort()
         for num in nums: 
             res += [ i + [num] for i in res]
         return res
