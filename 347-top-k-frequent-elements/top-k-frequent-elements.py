9ea737ef44a52250bc896b718f9eeed194c60a5b
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2025-05-08 22:27:58 +0800

    Time: 12 ms (46.08%) | Memory: 19.8 MB (20.37%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -5,44 +5,21 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-        res = []
-        count = Counter(nums)
-        freq = [[] for _ in range(len(nums)+1)]
-
-        for n, c in count.items():
-            freq[c].append(n)
-
-        for i in range(len(freq)-1, 0, -1):
-            for n in freq[i]:
-                res.append(n)
-                if len(res) == k:
-                    return res
-
-
-        res = []
-        count = Counter(nums)
-        freq = [[] for _ in range(len(nums)+1)]
-        for n, c in count.items():
-            freq[c].append(n)
+        count = defaultdict(int)
+        for n in nums:
+            count[n] += 1
         
-        for i in range(len(freq)-1, 0, -1):
-            for n in freq[i]:
-                res.append(n)
-                if len(res) == k:
-                    return res
+        bucket = [[] for i in range(len(nums)+1)]
+        for key, val in count.items():
+            bucket[val].append(key)
         
-        min_heap = []
-        look_up = Counter(nums)
-        res = []
-
-        for n, count in look_up.items():
-            if len(min_heap) >= k:
-                heapq.heappushpop(min_heap, (count, n))
-            else:
-                heapq.heappush(min_heap, (count, n))
-        
-        while k > 0:
-            res.append(heapq.heappop(min_heap)[1])
-            k -= 1
-        return res
-
+        result = []
+        c = 0
+        for b in bucket[::-1]:
+            for n in b:
+                result.append(n)
+                c += 1
+                if k == c:
+                    return result
+            
+            
