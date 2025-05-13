679ef45c0c8d052730242f192dfc837d901276ef
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2025-05-13 23:08:05 +0800

    Time: 0 ms (100.00%) | Memory: 14.2 MB (94.35%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.py
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -6,34 +6,14 @@
 class Solution(object):
     def reverseList(self, head):
         """
-        :type head: ListNode
-        :rtype: ListNode
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
         """
-        # 1 -> 2 -> 3 -> None
-        if not head or not head.next:
-            return head
-
-        # return the reverse_head
-        reverse_head = self.reverseList(head.next) # head = 2, reverse_head = 3
-
-        # recursion start to reverse
-        head.next.next = head # 2.next.next = 2 
-        # 1 -> 2 <-> 3
-        head.next = None # 2 -> None
-        # 1 -> 2 <- 3
-
-        return reverse_head # return 3
-
-
-        
-
-
-        # prev = None #(None)
-        # curr = head #(1)
-
-        # while curr:
-        #     next_node = curr.next #(2) / (3)
-        #     curr.next = prev #(->None) / (->1)
-        #     prev = curr #(None to 1) / (2)
-        #     curr = next_node #(2) / (3)
-        # return prev
+        prev = None
+        curr = head
+        while curr:
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+        return prev
