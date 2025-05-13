21b6b3942e86689d1393de96f8ca9295cfce68e8
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2025-05-13 21:28:50 +0800

    Time: 666 ms (79.59%) | Memory: 18.2 MB (85.28%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -11,19 +11,18 @@ class Solution(object):
             if i > 0 and nums[i] == nums[i-1]:
                 i += 1
                 continue
+                
             l = i + 1
             r = len(nums) - 1
             while l < r:
                 threeSum = a + nums[l] + nums[r]
-                if threeSum > 0:
-                    r -= 1
-                elif threeSum < 0:
+                if threeSum < 0:
                     l += 1
+                elif threeSum > 0:
+                    r -= 1 
                 else:
                     res.append([a, nums[l], nums[r]])
                     l += 1
-                    while l < r and nums[l] == nums[l-1]:
+                    while nums[l] == nums[l-1] and l < len(nums) - 1:
                         l += 1
         return res
-                
-
