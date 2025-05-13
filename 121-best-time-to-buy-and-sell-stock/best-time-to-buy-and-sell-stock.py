4c4807a3ed396752f630e777e068110486d1c573
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2025-05-13 21:56:00 +0800

    Time: 129 ms (26.94%) | Memory: 19.1 MB (71.97%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -4,18 +4,17 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        # prices = [7,1,5,3,6,4]
         maxProfit = 0
         l = 0
         r = 0
-        while r < len(prices):
-            if prices[l] <= prices[r]: # check if it is a valid window, keep moves the right pointer
-                currProfit = prices[r] - prices[l]
-                maxProfit = max(currProfit, maxProfit)
-            else: # it is not a valid window move the left pointer
-                l = r
-            r += 1
-        return maxProfit
-        # TC: O(n) / SC: O(1)
 
-            
+        while r < len(prices):
+            if prices[l] <= prices[r]:
+                currentProfit = prices[r] - prices[l]
+                maxProfit = max(currentProfit, maxProfit)
+                r += 1
+            else:
+                l = r
+                r += 1
+        return maxProfit                
+                
