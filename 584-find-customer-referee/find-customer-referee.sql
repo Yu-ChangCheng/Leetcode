d2fb51f6d9cd7c6c89cd88b71e7b00aed4e0edb4
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2025-06-11 11:25:03 -0400

    Time: 450 ms (91.16%) | Memory: 0B (100.00%) - LeetSync

--- 584-find-customer-referee/find-customer-referee.sql
+++ 584-find-customer-referee/find-customer-referee.sql
@@ -1,8 +1,2 @@
 # Write your MySQL query statement below
-SELECT name
-FROM Customer
--- # SOLUTION 1  - MOST EFFICIENT 
--- WHERE 
---     IFNULL(referee_id,0) <> 2;
-#SOLUTOIN 2    
-WHERE referee_id != 2 OR referee_id is NULL
+select name from Customer where referee_id !=2 or referee_id is null
