6aff68c6a756b3fac764aaba68dd547cf88157ce
Yu-Chang Cheng <128564396+Yu-ChangCheng@users.noreply.github.com>
2025-07-31 00:36:31 -0400

    Time: 8 ms (41.96%) | Memory: 18.2 MB (11.77%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.py
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -7,33 +7,16 @@
 class Solution(object):
     def isBalanced(self, root):
         """
-        :type root: TreeNode
+        :type root: Optional[TreeNode]
         :rtype: bool
         """
         
-
-        def dfs(node):
-            if not node:
+        def dfs(root):
+            if not root:
                 return [True, 0]
             
-            left = dfs(node.left)
-            right = dfs(node.right)
+            left, right = dfs(root.left), dfs(root.right)
+            balanced = left[0] and right[0] and abs(left[1]-right[1]) <= 1
+            return [balanced, 1 + max(left[1], right[1])]
 
-            isBlanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1
-            return [isBlanced, 1 + max(left[1], right[1])]
-        
         return dfs(root)[0]
-
-
-    def dfs(self, node):
-        if not node:
-            return [True, 0] # [True, level]
-
-        left = self.dfs(node.left)
-        right = self.dfs(node.right)
-
-        isBlanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1
-
-        return [isBlanced, 1 + max(left[1], right[1])]
-
-        return self.dfs(root)[0]
